c3dfa91c4a8c73b66be6aacd3dd1e49dd0223153
tylau0822 <34152633+tylau0822@users.noreply.github.com>
2024-07-24 17:07:17 -0400

    Add login page

--- composeApp/src/commonMain/kotlin/Color.kt
+++ composeApp/src/commonMain/kotlin/Color.kt
@@ -1,3 +1,8 @@
 import androidx.compose.ui.graphics.Color
 
-val PrimaryTeal = Color(0xFF53B175) 
+val PrimaryTeal = Color(0xFF53B175)
+var TextFieldBg = Color(0xFFF6F6F6)
+var GrayBorder = Color(0xFFE8E8E8)
+var Black = Color(0xFF000000)
+var Blue = Color(0xFF1877F2)
+var Gray = Color(0xFF666666)
